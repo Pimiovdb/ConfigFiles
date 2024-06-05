9f1147cf67108e512100cc0690d646ed52e61fc1
Pimiovdb <pim.noordhoek@hotmail.com>
2024-06-05 10:35:50 +0200

    config file: Windows Terminal

---
+++ WINTerminalConfig/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+oh-my-posh init pwsh --config 'C:\Users\Pim\AppData\Local\Programs\oh-my-posh\themes\kali.omp.json' | Invoke-Expression
+
+Set-PSReadLineOption -PredictionSource History -PredictionViewStyle ListView
+
+#34de4b3d-13a8-4540-b76d-b9e8d3851756 PowerToys CommandNotFound module
+
+Import-Module "C:\Program Files\PowerToys\WinUI3Apps\..\WinGetCommandNotFound.psd1"
+#34de4b3d-13a8-4540-b76d-b9e8d3851756
